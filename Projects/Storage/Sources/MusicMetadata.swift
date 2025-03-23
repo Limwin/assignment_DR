f91a9e556dd61882053fe8a7ce2b500c66c7f175
seunghyeok lim <seunghyeoklim@seunghyeokui-MacBookAir.local>
2025-03-23 14:43:54 +0900

    feat: StorageHandler 구현
    - Storage 내의 mp3 정보를 불러와 metadata, duration 정보를 반환하는 객체 구현

---
+++ Projects/Storage/Sources/MusicMetadata.swift
@@ -0,0 +1,14 @@
+//
+//  MusicMetadata.swift
+//  Storage
+//
+//  Created by seunghyeok lim on 3/23/25.
+//
+
+import Foundation
+import AVFoundation
+
+public struct AssetInfo {
+    public let items: [AVMetadataItem]
+    public let duration: CMTime
+}
